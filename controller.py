15b195cb1c528f4c72c0e6a50c6ac20c78d97ac7
AlexandreDelesse <alex.delesse01@gmail.com>
2021-05-07 20:40:40 +0200

    few changes

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
-from model import Model
+from beacon import Beacon
 from view import View
 from buzzer import Buzzer
 from persistance import Persistance
-import threading
+import threading, time
 
 class Controller:
 	
@@ -13,10 +13,10 @@ class Controller:
 	alarme = True
 
 	def __init__(self):
-		self.model = Model()
+		self.beacon = Beacon()
 		self.view = View(self)
 		self.persistance = Persistance()
-		self.bt_thread = threading.Thread(target=self.display_bluetooth_value)
+		self.bt_thread = threading.Thread(target=self.refreshDataInView)
 		self.view.alarme_var.set(self.ALARME_ON)
 		self.buzzer = Buzzer()
 		
@@ -25,23 +25,35 @@ class Controller:
 		self.bt_thread.start()
 		self.view.main()
 
-	def display_bluetooth_value(self):
+	def refreshDataInView(self):
 		while(1):
-			result = self.model.get_bluetooth_value()
-			self.view.value_var.set(str(result) + " °C")
-			self.persistance.write_in_file(result)
+			self.beacon.refreshTemperature()
+			temperature = self.beacon.getTemperature()
+			temperatureTime = self.beacon.getTemperatureTime()
+			beaconName = self.beacon.getBeaconName()
+
+			self.view.value_var.set(str(temperature) + " °C")
+			self.persistance.write_in_file(beaconName, temperatureTime, temperature)
+
+			time.sleep(5)
 
 
 	def on_off_alarme(self):
-		if (self.alarme == True):
-			self.alarme = False
+		if (self.beacon.isAlarmeActivated == True):
+			self.beacon.isAlarmeActivated = False
 			self.view.alarme_var.set(self.ALARME_OFF)
 			print("alarme OFF")
 		else:
-			self.alarme = True
+			self.beacon.isAlarmeActivated = True
 			self.view.alarme_var.set(self.ALARME_ON)
 			print("alarme ON")
 
 if __name__== '__main__':
 	app = Controller()
 	app.main()
+
+
+
+
+
+	
--- persistance.py
+++ persistance.py
@@ -6,15 +6,20 @@ class Persistance:
 	def __init__(self):
 		pass
 	
-	def write_in_file(self, value):
+	def write_in_file(self, beaconName, temperatureTime, temperature):
 		date = datetime.now()
 		date_mod = date.strftime("%d_%m_%Y.csv")
 
-		if (os.path.exists("./sauvegardes/" + date_mod)):
+		if (os.path.exists("./sauvegardes/" + beaconName + "_" + date_mod)):
 			print("fichier existe") 
 		else:
-			with open("./sauvegardes/" + date_mod, 'a') as csvfile:
+			with open("./sauvegardes/" + beaconName + "_" + date_mod, 'a') as csvfile:
 				newwriter = csv.writer(csvfile)
 				newwriter.writerow(['nom', 'date_heure', 'temperature'])
-		c = csv.writer(open("./sauvegardes/" + date_mod, "a"))
-		c.writerow(['CT 801', date , value])
+		c = csv.writer(open("./sauvegardes/" + beaconName + "_" + date_mod, "a"))
+		c.writerow([beaconName, temperatureTime , temperature])
+
+
+
+
+
